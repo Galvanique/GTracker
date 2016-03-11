c62ea7a44bd1993c40983ff1e461db4b76582ba1
Tyler Caldwell <tylercaldwell217@gmail.com>
2016-03-10 21:35:10 -0500

    added login using template username: foo@example.com password: hello

--- app/src/main/java/galvanique/client/GetHelpActivity.java
+++ app/src/main/java/galvanique/client/GetHelpActivity.java
@@ -20,6 +20,9 @@ public class GetHelpActivity extends Activity {
     private Button getLTHelpButton;
     private TextView copingStratStr;
 
+    /**
+     * @see galvanique.db.CopingStrategyDAO#getRandomCopingStrategy(int longTerm)
+     */
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +36,6 @@ public class GetHelpActivity extends Activity {
             public void onClick(View v) {
                 CopingStrategyDAO dao = new CopingStrategyDAO(getApplicationContext());
                 dao.openRead();
-                // TODO query db, display strategy
                 CopingStrategyLog cs = dao.getRandomCopingStrategy(0); // 0 for short term -- sqlite doesn't have booleans
                 dao.close();
                 copingStratStr.setText(cs.getCopingStrategy());
@@ -46,7 +48,6 @@ public class GetHelpActivity extends Activity {
             public void onClick(View v) {
                 CopingStrategyDAO dao = new CopingStrategyDAO(getApplicationContext());
                 dao.openRead();
-                // TODO query db, display strategy
                 CopingStrategyLog cs = dao.getRandomCopingStrategy(1); // 1 for long term
                 Log.d("random cs is", cs.getCopingStrategy());
                 dao.close();
@@ -56,19 +57,10 @@ public class GetHelpActivity extends Activity {
         copingStratStr = (TextView) findViewById(R.id.CopingStratStr);
     }
 
-    // TODO create a simple layout for get help per the UI diagram
-    // 1. A button for regulate distress
-        // Pressing this button (onClick) fires a method that pulls a random coping strategy from our
-        // (currently nonexistent) coping strategy database and displays it in an alert box with an
-        // "okay" button. You could also just make a text value in the layout and keep it blank until
-        // the user asks for a coping strategy.
-    // 2. A button for adapt behavior
-        // Same thing as above, except this one only does long term and that one only does short term.
-    // 3. Mark the time, send a push notification asking the user to rate the strategy's effectiveness
-        // once enough time has passed. Update the database with the rating.
 
-    // see CopingStrategyDAO.getRandomCopingStrategy() -- it won't work if you try to test it because
-    // the database doesn't exist yet / it's initialized to be empty anyway, but it's the method you
-    // would use
+    /* TODO
+        Mark the time, send a push notification asking the user to rate the strategy's effectiveness
+        once enough time has passed. Update the database with the rating.
+    */
 
 }
--- app/src/main/java/galvanique/client/PreferencesActivity.java
+++ app/src/main/java/galvanique/client/PreferencesActivity.java
@@ -2,11 +2,46 @@ package galvanique.client;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreferencesActivity extends Activity {
+
+    private Button buttonBackground, buttonGSR, buttonNotifications;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+
+        /* Background */
+        buttonBackground = (Button) findViewById(R.id.buttonBackground);
+        buttonBackground.setOnClickListener(new View.OnClickListener() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+        /* GSR */
+        buttonGSR = (Button) findViewById(R.id.buttonGSR);
+        buttonGSR.setOnClickListener(new View.OnClickListener() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+        /* Notifications */
+        buttonNotifications = (Button) findViewById(R.id.buttonNotifications);
+        buttonNotifications.setOnClickListener(new View.OnClickListener() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
     }
 }
