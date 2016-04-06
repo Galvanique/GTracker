3711c176fcef8767241f42a8597acc1a2b4f70b7
KaiGeffen <geffenkai@gmail.com>
2016-04-06 16:16:30 -0400

    Implemented CopingStrategy and MoodDAO

--- app/src/main/java/galvanique/db/dao/MoodDAO.java
+++ app/src/main/java/galvanique/db/dao/MoodDAO.java
@@ -1,5 +1,82 @@
 package galvanique.db.dao;
 
-public class MoodDAO {
-    // TODO(kgeffen) Implement
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import galvanique.db.entities.Mood;
+
+public class MoodDAO extends GeneralDAO {
+
+    // --------------------------------------------
+    // SCHEMA
+    // --------------------------------------------
+
+    public static String TABLE_NAME = "moodLog";
+
+    public static final String TAG = "MoodLogDAO";
+
+    public static final String CNAME_ID = "_id";
+    public static final String CNAME_NAME = "name";
+
+    public static final String[] PROJECTION = {
+            CNAME_ID,
+            CNAME_NAME
+    };
+
+    public final static int CNUM_ID = 0;
+    public final static int CNUM_NAME= 1;
+
+    public static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
+            CNAME_ID + " INTEGER PRIMARY KEY, " +
+            CNAME_NAME + " TEXT" +
+            ");";
+
+    // --------------------------------------------
+    // QUERIES
+    // --------------------------------------------
+
+    private final static String WHERE_ID = CNAME_ID + "=?";
+
+    // --------------------------------------------
+    // LIVECYCLE
+    // --------------------------------------------
+
+    public MoodDAO(Context context) {
+        super(context);
+    }
+
+    // --------------------------------------------
+    // QUERY IMPLEMENTATIONS
+    // --------------------------------------------
+
+    public Mood getMoodById(int id) {
+        Cursor c = db.query(
+                TABLE_NAME,
+                PROJECTION,
+                WHERE_ID,
+                new String[]{id + ""},
+                null,
+                null,
+                null);
+        return cursor2mood(c);
+    }
+
+    // --------------------------------------------
+    // MOOD-CURSOR TRANSFORMATION UTILITIES
+    // --------------------------------------------
+
+    private static Mood cursor2mood(Cursor c) {
+        c.moveToFirst();
+        Mood r = new Mood();
+        r.id = c.getInt(CNUM_ID);
+        r.name = c.getString(CNUM_NAME);
+        return r;
+    }
+
 }
--- app/src/main/java/galvanique/db/entities/CopingStrategy.java
+++ app/src/main/java/galvanique/db/entities/CopingStrategy.java
@@ -1,5 +1,47 @@
 package galvanique.db.entities;
 
 public class CopingStrategy {
-    // TODO(kgeffen) Implement
+
+    public int id = -1;
+    public String name;
+
+    /**
+     * Constructor without id
+     *
+     * @param name
+     */
+
+    public CopingStrategy(String name) {
+        this.name= name;
+    }
+
+    /**
+     * Constructor with report id
+     *
+     * @param id
+     * @param name
+     */
+
+    public CopingStrategy(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Empty Constructor
+     */
+
+    public CopingStrategy() { }
+
+    public boolean equals(Mood r) {
+        return this.id == r.id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
