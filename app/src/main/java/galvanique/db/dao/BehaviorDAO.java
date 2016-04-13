ea8258c7aa503d9af231cd19a388c36853ae63da
Tyler Caldwell <tylercaldwell217@gmail.com>
2016-04-13 12:43:35 -0400

    changed columns in trigger, belief, behavior tables to reflect sds

--- app/src/main/java/galvanique/db/dao/BehaviorDAO.java
+++ app/src/main/java/galvanique/db/dao/BehaviorDAO.java
@@ -22,23 +22,19 @@ public class BehaviorDAO extends GeneralDAO {
 
     public static final String CNAME_ID = "_id";
     public static final String CNAME_NAME = "name";
-    public static final String CNAME_STRING = "string"; 
 
     public static final String[] PROJECTION = {
     	CNAME_ID,
-    	CNAME_NAME,
-        CNAME_STRING
+    	CNAME_NAME
     };
 
     public final static int CNUM_ID = 0;
     public final static int CNUM_NAME = 1;
-    public final static int CNUM_STRING = 2;
 
 
     public static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
         CNAME_ID + " INTEGER PRIMARY KEY, " +
         CNAME_NAME + " TEXT" +
-        CNAME_STRING + " TEXT" +
         ");";
 
     // --------------------------------------------
@@ -46,7 +42,7 @@ public class BehaviorDAO extends GeneralDAO {
     // --------------------------------------------
 
     private final static String WHERE_ID = CNAME_ID + "=?";
-    private final static String WHERE_STRING = CNAME_STRING + "=?";
+    private final static String WHERE_NAME = CNAME_NAME + "=?";
 
 
     // --------------------------------------------
@@ -84,7 +80,7 @@ public class BehaviorDAO extends GeneralDAO {
         Cursor c = db.query(
                 TABLE_NAME,
                 PROJECTION,
-                WHERE_STRING,
+                WHERE_NAME,
                 new String[]{s + ""},
                 null,
                 null,
--- app/src/main/java/galvanique/db/dao/BeliefDAO.java
+++ app/src/main/java/galvanique/db/dao/BeliefDAO.java
@@ -21,22 +21,18 @@ public class BeliefDAO extends GeneralDAO {
 
     public static final String CNAME_ID = "_id";
     public static final String CNAME_NAME = "name";
-    public static final String CNAME_STRING = "string"; 
 
     public static final String[] PROJECTION = {
     	CNAME_ID,
-    	CNAME_NAME,
-        CNAME_STRING
+    	CNAME_NAME
     };
 
     public final static int CNUM_ID = 0;
     public final static int CNUM_NAME = 1;
-    public final static int CNUM_STRING = 2;
 
     public static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
         CNAME_ID + " INTEGER PRIMARY KEY, " +
         CNAME_NAME + " TEXT" +
-        CNAME_STRING + " TEXT" +
         ");";
 
     // --------------------------------------------
@@ -44,7 +40,7 @@ public class BeliefDAO extends GeneralDAO {
     // --------------------------------------------
 
     private final static String WHERE_ID = CNAME_ID + "=?";
-    private final static String WHERE_STRING = CNAME_STRING + "=?";
+    private final static String WHERE_NAME = CNAME_NAME + "=?";
 
 
     // --------------------------------------------
@@ -82,7 +78,7 @@ public class BeliefDAO extends GeneralDAO {
         Cursor c = db.query(
                 TABLE_NAME,
                 PROJECTION,
-                WHERE_STRING,
+                WHERE_NAME,
                 new String[]{s + ""},
                 null,
                 null,
