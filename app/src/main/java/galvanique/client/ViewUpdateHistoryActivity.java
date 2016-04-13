4d7238bee7f75e7852e5e69c0575721e49a3cc9d
Tyler Caldwell <tylercaldwell217@gmail.com>
2016-04-13 16:16:03 -0400

    set up view/update history activity

--- app/src/main/java/galvanique/client/ViewUpdateHistoryActivity.java
+++ app/src/main/java/galvanique/client/ViewUpdateHistoryActivity.java
@@ -3,10 +3,20 @@ package galvanique.client;
 import android.app.Activity;
 import android.os.Bundle;
 
+import galvanique.db.dao.MoodLogDAO;
+import galvanique.db.entities.MoodLog;
+
 public class ViewUpdateHistoryActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_update_history);
+
+        // Read all rows of MoodLog table into an array of MoodLog objects
+        MoodLogDAO dbMoodLog = new MoodLogDAO(getApplicationContext());
+        dbMoodLog.openRead();
+        MoodLog[] moodLogs = dbMoodLog.getAllMoods();
+        dbMoodLog.close();
+        // TODO-dave,clark display this array as a table with the information we mention in the SDS
     }
 }
