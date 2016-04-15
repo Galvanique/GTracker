4ea71d9119ecc7ebe1e1940e7a0da9fc18720bff
Tyler Caldwell <tylercaldwell217@gmail.com>
2016-04-14 20:20:02 -0400

    added table layout object to view/update history

--- app/src/main/java/galvanique/client/ViewUpdateHistoryActivity.java
+++ app/src/main/java/galvanique/client/ViewUpdateHistoryActivity.java
@@ -2,15 +2,20 @@ package galvanique.client;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 import galvanique.db.dao.MoodLogDAO;
 import galvanique.db.entities.MoodLog;
 
 public class ViewUpdateHistoryActivity extends Activity {
+    TableLayout table;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_update_history);
+
+        table = (TableLayout) findViewById(R.id.tableLayout);
 
         // Read all rows of MoodLog table into an array of MoodLog objects
         MoodLogDAO dbMoodLog = new MoodLogDAO(getApplicationContext());
