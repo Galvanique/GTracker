1ea601eead677e47effc816cb1d52dff0fd63e57
KaiGeffen <geffenkai@gmail.com>
2016-04-14 21:24:35 -0400

    Remade the test dir, added stub for first tests

---
+++ app/src/test/java/galvanique/db/entities/GsrLogTest.java
@@ -0,0 +1,21 @@
+package galvanique.db.entities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TODO(kgeffen) write words
+ */
+public class GsrLogTest {
+    @Test
+    public void sanity_check() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void foo() throws Exception {
+        GsrLog bar;
+        assert(true);
+    }
+}
