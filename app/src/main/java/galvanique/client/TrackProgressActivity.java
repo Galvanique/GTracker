73d9fd28fb0c60133f75d2d9f67a8ce4d892f618
David Whitaker <dwhitaker@umass.edu>
2016-04-19 19:46:14 -0400

    Created skeleton for the moodlog XYPlot. Still need to fix the axes to be readable and also create series for each mood

--- app/src/main/java/galvanique/client/TrackProgressActivity.java
+++ app/src/main/java/galvanique/client/TrackProgressActivity.java
@@ -3,8 +3,19 @@ package galvanique.client;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.app.Activity;
+import android.graphics.Color;
+
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.*;
+import java.util.Arrays;
+
+import galvanique.db.dao.MoodLogDAO;
+import galvanique.db.entities.MoodLog;
 
 public class TrackProgressActivity extends AppCompatActivity {
+    private XYPlot mySimpleXYPlot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +23,51 @@ public class TrackProgressActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        MoodLogDAO dbMoodLog = new MoodLogDAO(getApplicationContext());
+        dbMoodLog.openRead();
+
+        MoodLog[] moodLogArray = dbMoodLog.getMoodsOverTime();
+        dbMoodLog.close();
+
+        //for()
+        //TODO Set the different moods into different series
+        // initialize our XYPlot reference:
+        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+        //
+        mySimpleXYPlot.setRangeBottomMax(-50);
+        // Create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                "Series1");                             // Set the display title of the series
+
+        // same as above
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // Create a formatter to use for drawing a series using LineAndPointRenderer:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(
+                Color.rgb(0, 200, 0),                   // line color
+                Color.rgb(0, 100, 0),                   // point color
+                null,                                   // fill color (none)
+                new PointLabelFormatter(Color.WHITE));                           // text color
+
+        // add a new series' to the xyplot:
+        mySimpleXYPlot.addSeries(series1, series1Format);
+
+        // same as above:
+        mySimpleXYPlot.addSeries(series2,
+                new LineAndPointFormatter(
+                        Color.rgb(0, 0, 200),
+                        Color.rgb(0, 0, 100),
+                        null,
+                        new PointLabelFormatter(Color.WHITE)));
+
+        // reduce the number of range labels
+        mySimpleXYPlot.setTicksPerRangeLabel(3);
     }
 }
