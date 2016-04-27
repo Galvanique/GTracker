c554c38e065a24e39b2caeb6a42c27b8c6b13ed2
Tyler Caldwell <tylercaldwell217@gmail.com>
2016-04-27 14:11:36 -0400

    got rid of settings button in splashscreen

--- app/src/main/java/galvanique/client/SplashScreenActivity.java
+++ app/src/main/java/galvanique/client/SplashScreenActivity.java
@@ -29,8 +29,6 @@ public class SplashScreenActivity extends AppCompatActivity
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        final String[] values = getResources().getStringArray(R.array.colorcode_array);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,28 +57,6 @@ public class SplashScreenActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
